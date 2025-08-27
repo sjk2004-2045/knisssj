40a550956ebd34049290a30bfc985cf58efe6ac5
Sylvia Kniss <sjkniss2004@gmail.com>
2025-08-27 17:24:06 -0400

    started stuff

---
+++ smart_wd.R
@@ -0,0 +1,16 @@
+  kniss_wd <- function (repo, folder=NULL) {
+    if(missing(folder))
+    {
+      setwd(print(paste(if (Sys.info()[["sysname"]]=="Windows") {("C:/GitHub")} else {
+        if (Sys.info()[["sysname"]]=="Darwin"){("/Users/sylviakniss/Documents/GitHub")} else ("for Linux run: setwd('/home/[INSERT YOUR USERNAME]/GitHub')")
+      },"/",repo, sep = "")))}
+    else 
+     
+    {
+      setwd(print(paste(if (Sys.info()[["sysname"]]=="Windows") {("C:/GitHub")} else {
+        if (Sys.info()[["sysname"]]=="Darwin"){("/Users/sylviakniss/Documents/GitHub")} else ("for Linux run: setwd('/home/[INSERT YOUR USERNAME]/GitHub')")
+      },"/",repo, "/", folder, sep = "")))}
+  }
+
+
+kniss_wd(repo = "knisssj", folder = "code")
